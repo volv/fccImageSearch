1e22cf9c964fd3648bf7089548e46b5ba5b4a4a7
volv <steven@bigham.com>
2016-01-25 18:53:43 +0000

    Prune

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,11 @@ module.exports = function (app, passport) {
 		.get(function(req, res, next) {
 	       res.json(req.params.date);
 	       console.log(req.params.date);
+	       //next();
 		})
+		
+	app.get('/', function(req, res) {
+		res.sendFile(process.cwd() + '/public/index.html');
+	})	
 
 };
